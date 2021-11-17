d97a6698f94cd5af57855c99168ceb3b3f0a8d09
geekysrm <soumyacool2012@gmail.com>
2021-11-18 03:46:31 +0530

    fix bug

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -106,13 +106,15 @@ export const NotionPage: React.FC<types.PageProps> = ({
 
   const title = getBlockTitle(block, recordMap) || site.name
 
-  console.log('notion page', {
-    isDev: config.isDev,
-    title,
-    pageId,
-    rootNotionPageId: site.rootNotionPageId,
-    recordMap
-  })
+  if(config?.isDev) {
+    console.log('notion page', {
+      isDev: config.isDev,
+      title,
+      pageId,
+      rootNotionPageId: site.rootNotionPageId,
+      recordMap
+    })
+  }
 
   if (!config.isServer) {
     // add important objects to the window global for easy debugging
@@ -165,6 +167,10 @@ export const NotionPage: React.FC<types.PageProps> = ({
   } else {
     pageAside = <PageSocial />
   }
+
+  React.useEffect(() => {
+    darkMode.disable()
+  }, [])
 
   return (
     <TwitterContextProvider

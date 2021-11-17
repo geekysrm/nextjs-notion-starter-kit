4db7a2c33ab171bbeeef21c7ce6b831a2f225f4d
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2021-11-18 01:16:16 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '9d7e9163785242ffa79314d1db62dd04',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Soumya Ranjan Mohanty',
+  domain: 'soumya.dev',
+  author: 'Soumya Ranjan Mohanty',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'Personal website of Soumya Ranjan Mohanty aka geekySRM, Software Developer from India',
+  socialImageTitle: 'Soumya Ranjan Mohanty',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'geekysrm',
+  github: 'geekysrm',
+  linkedin: 'geekysrm',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
